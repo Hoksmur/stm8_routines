5e4c95a45007a624386b38978c20bf4d23a4782d
Hoksmur <dragon-78@yandex.ru>
2014-11-20 13:57:45 +0700

    Update owire.c

--- owire.c
+++ owire.c
@@ -8,55 +8,54 @@
 uint8_t iStateSave;
 
 /* do CRC8 table */
-const unsigned char OW_crc8_table[] = {
-	0, 94,188,226, 97, 63,221,131,194,156,126, 32,163,253, 31, 65,
-	157,195, 33,127,252,162, 64, 30, 95, 1,227,189, 62, 96,130,220,
-	35,125,159,193, 66, 28,254,160,225,191, 93, 3,128,222, 60, 98,
-	190,224, 2, 92,223,129, 99, 61,124, 34,192,158, 29, 67,161,255,
-	70, 24,250,164, 39,121,155,197,132,218, 56,102,229,187, 89, 7,
-	219,133,103, 57,186,228, 6, 88, 25, 71,165,251,120, 38,196,154,
-	101, 59,217,135, 4, 90,184,230,167,249, 27, 69,198,152,122, 36,
-	248,166, 68, 26,153,199, 37,123, 58,100,134,216, 91, 5,231,185,
-	140,210, 48,110,237,179, 81, 15, 78, 16,242,172, 47,113,147,205,
-	17, 79,173,243,112, 46,204,146,211,141,111, 49,178,236, 14, 80,
-	175,241, 19, 77,206,144,114, 44,109, 51,209,143, 12, 82,176,238,
-	50,108,142,208, 83, 13,239,177,240,174, 76, 18,145,207, 45,115,
-	202,148,118, 40,171,245, 23, 73, 8, 86,180,234,105, 55,213,139,
-	87, 9,235,181, 54,104,138,212,149,203, 41,119,244,170, 72, 22,
-	233,183, 85, 11,136,214, 52,106, 43,117,151,201, 74, 20,246,168,
-	116, 42,200,150, 21, 75,169,247,182,232, 10, 84,215,137,107, 53};
+const unsigned char W1_crc8_table[] = {
+	0, 94,188,226, 97, 63,221,131,194,156,126, 32,163,253, 31, 65,	// 0
+	157,195, 33,127,252,162, 64, 30, 95, 1,227,189, 62, 96,130,220,	// 16
+	35,125,159,193, 66, 28,254,160,225,191, 93, 3,128,222, 60, 98,	// 32
+	190,224, 2, 92,223,129, 99, 61,124, 34,192,158, 29, 67,161,255,	// 48
+	70, 24,250,164, 39,121,155,197,132,218, 56,102,229,187, 89, 7,	// 64
+	219,133,103, 57,186,228, 6, 88, 25, 71,165,251,120, 38,196,154,	// 80
+	101, 59,217,135, 4, 90,184,230,167,249, 27, 69,198,152,122, 36,	// 96
+	248,166, 68, 26,153,199, 37,123, 58,100,134,216, 91, 5,231,185,	// 112
+	140,210, 48,110,237,179, 81, 15, 78, 16,242,172, 47,113,147,205,	//128
+	17, 79,173,243,112, 46,204,146,211,141,111, 49,178,236, 14, 80,	// 144
+	175,241, 19, 77,206,144,114, 44,109, 51,209,143, 12, 82,176,238,	//160
+	50,108,142,208, 83, 13,239,177,240,174, 76, 18,145,207, 45,115,	// 176
+	202,148,118, 40,171,245, 23, 73, 8, 86,180,234,105, 55,213,139,	// 192
+	87, 9,235,181, 54,104,138,212,149,203, 41,119,244,170, 72, 22,	// 208
+	233,183, 85, 11,136,214, 52,106, 43,117,151,201, 74, 20,246,168,	// 224
+	116, 42,200,150, 21, 75,169,247,182,232, 10, 84,215,137,107, 53};	// 240
 
-
-unsigned char OW_rxBit(void)
+unsigned char W1_rxBit(void)
 {
 unsigned char out;
 	_asm("nop");
-	OW_PORT->ODR &= SFR( ~(1<<OW_PIN) ); // low
+	W1_PORT->ODR &= SFR( ~(1<<W1_PIN) ); // low
 	_delay_us( A);
-	OW_PORT->ODR |= 1<<OW_PIN;
+	W1_PORT->ODR |= 1<<W1_PIN;
 	_delay_us( E);
-	out = SFR( ( OW_PORT->IDR & 1<<OW_PIN )  ? 0x01 : 0x00 );
+	out = SFR( ( W1_PORT->IDR & 1<<W1_PIN )  ? 0x01 : 0x00 );
 	_delay_us( F);
 	return (out);
 }
 
 
-unsigned char OW_txBit( unsigned char bit)
+unsigned char W1_txBit( unsigned char bit)
 {
         if (bit != 0)
         {
                 // Write '1' bit
-                OW_PORT->ODR &= SFR( ~(1<<OW_PIN)); // Drives DQ low
-				//if (bit_is_set(OW_IN, OW_PIN) ) { return 0;}
+                W1_PORT->ODR &= SFR( ~(1<<W1_PIN)); // Drives DQ low
+				//if (bit_is_set(W1_IN, W1_PIN) ) { return 0;}
                 _delay_us(A);
-                OW_PORT->ODR |=  1<<OW_PIN; // Releases the bus
+                W1_PORT->ODR |=  1<<W1_PIN; // Releases the bus
                 _delay_us(B); // Complete the time slot and 10us recovery
         } else {
                 // Write '0' bit
-                OW_PORT->ODR &= SFR(~(1<<OW_PIN)); // Drives DQ low
-				//if (bit_is_set(OW_IN, OW_PIN) ) { return 0;}
+                W1_PORT->ODR &= SFR(~(1<<W1_PIN)); // Drives DQ low
+				//if (bit_is_set(W1_IN, W1_PIN) ) { return 0;}
                 _delay_us(C);
-                OW_PORT->ODR |=  1<<OW_PIN; // Releases the bus
+                W1_PORT->ODR |=  1<<W1_PIN; // Releases the bus
                 _delay_us(D);
         }
 	return ( 1 ); // compatability CodeVision
@@ -72,18 +71,18 @@ iStateSave = ATOMIC_BEGIN();
 #endif	
 /* Init IO and reset presence pulse */
 	// select Open drain output, fast mode
-	OW_PORT->DDR |= SFR(  1<<OW_PIN);
-//	OW_PORT->CR1 |= SFR(1<<OW_PIN);
-	OW_PORT->CR1 &= SFR(~(1<<OW_PIN));
-	OW_PORT->CR2 |= SFR(  1<<OW_PIN);
+	W1_PORT->DDR |= SFR(  1<<W1_PIN);
+//	W1_PORT->CR1 |= SFR(1<<W1_PIN);
+	W1_PORT->CR1 &= SFR(~(1<<W1_PIN));
+	W1_PORT->CR2 |= SFR(  1<<W1_PIN);
 
-	OW_PORT->ODR &= SFR(~(1<<OW_PIN));
+	W1_PORT->ODR &= SFR(~(1<<W1_PIN));
 	_delay_us(H);
 
-	OW_PORT->ODR |=   1<<OW_PIN;
+	W1_PORT->ODR |=   1<<W1_PIN;
 	_delay_us(I);
-//	out = bit_is_clean( (OW_PORT->IDR), OW_PIN) ? 0x01 : 0x00 ;
-	out = (uint8_t)( !( OW_PORT->IDR & 1<<OW_PIN)  ? 0x01 : 0x00 );
+//	out = bit_is_clean( (W1_PORT->IDR), W1_PIN) ? 0x01 : 0x00 ;
+	out = (uint8_t)( !( W1_PORT->IDR & 1<<W1_PIN)  ? 0x01 : 0x00 );
 	_delay_us(J);
 #if INTDE
 ATOMIC_END( iStateSave );
@@ -104,7 +103,7 @@ iStateSave = ATOMIC_BEGIN();
 	_delay_us(60);
 	for (j = 0;j<8;j++) {
 		out >>= 1;
-		out |= SFR( OW_rxBit() ? 0x80 : 0 );
+		out |= SFR( W1_rxBit() ? 0x80 : 0 );
 	}
 #if INTDE
 ATOMIC_END( iStateSave );
@@ -123,10 +122,10 @@ iStateSave = ATOMIC_BEGIN();
 #endif
 	for(i=0; i<8;i++) {
 		if ( (data & (0x01<<i)) != 0 ) {
-			OW_txBit(1);
+			W1_txBit(1);
 		}
 		else {
-			OW_txBit(0);
+			W1_txBit(0);
 		}
 //		_asm("nop");
 	}
@@ -137,6 +136,7 @@ ATOMIC_END( iStateSave );
 }
 
 void storeBit( unsigned char  bitIdx, unsigned char bitVal, unsigned char *p)
+// write bitVal on bitIdx by *p
 {
 unsigned char ch;
 ch = *(p + (bitIdx>>3));	// byte address
@@ -148,21 +148,20 @@ if (bitVal) {
 *(p + (bitIdx>>3)) = ch;	// save actual byte
 }
 
-unsigned char w1_search(unsigned char cmd, unsigned char *p)
+w1_error_t w1_search(unsigned char cmd, unsigned char *p)
+// rerun
 // using application note AN187 
 // http://pdfserv.maximintegrated.com/en/an/AN187.pdf
 {
 static unsigned char lastZero;
-static unsigned char lastDevFlg = 0;
 static unsigned char lastConflict = 0;
 unsigned char id_bit_num;
 unsigned char id_bit, id_cmp_bit;
 unsigned char bit_to_write;
 
-	if ( !w1_init() || (lastDevFlg != 0) ) {	// no devices // previous ID was the last
+	if ( !w1_init() ) {	// no devices // previous ID was the last
 		lastConflict = 0;
-		lastDevFlg = 0;
-		return( 0);
+		return( NODEV_PRESENT);
 	}
 	id_bit_num = 1;
 	lastZero = 0;
@@ -172,15 +171,25 @@ unsigned char bit_to_write;
 #if INTDE
 iStateSave = ATOMIC_BEGIN();
 #endif
-		id_bit     = OW_rxBit();
-		id_cmp_bit = OW_rxBit();
+		id_bit     = W1_rxBit();
+		id_cmp_bit = W1_rxBit();
 #if INTDE
 ATOMIC_END( iStateSave );
 #endif
 		if ( (id_bit != 0) && (id_cmp_bit != 0) ) {	// connection fault!
 			lastConflict = 0;
-			lastDevFlg = 0;
-			return( 0);
+			/*{
+				*p = 0x00;
+				*(p+1)=0x00;
+				*(p+2)=0x00;
+				*(p+3)=0x00;
+				*(p+4)=0x00;
+				*(p+5)=0x00;
+				*(p+6)=0x00;
+				*(p+7)=0x00;
+				storeBit( id_bit_num, 0x01, p);
+			}*/
+			return( CONN_FAULT);
 		} 
 		if ( (id_bit==0) && (id_cmp_bit==0) ) {	// 00: different bits in devices
 			if ( id_bit_num == lastConflict) {
@@ -203,7 +212,7 @@ ATOMIC_END( iStateSave );
 #if INTDE
 iStateSave = ATOMIC_BEGIN();
 #endif
-		OW_txBit(bit_to_write);
+		W1_txBit(bit_to_write);
 #if INTDE
 ATOMIC_END( iStateSave );
 #endif
@@ -212,13 +221,13 @@ ATOMIC_END( iStateSave );
 
 	lastConflict = lastZero;
 	if ( lastConflict == 0) {
-		lastDevFlg = 1;
+		return (LAST_DEV);
 	}
-	return( 1);
+	return( W1_OK);
 }
 
 
-unsigned char w1_dow_crc8(unsigned char *p, unsigned char n)
+unsigned char w1_dow_crc8(unsigned char *p, unsigned char count)
 {
 //unsigned char *ptr=(unsigned char *)p;
 unsigned char i;
@@ -226,8 +235,8 @@ unsigned char crc = 0;
 
 	i=0;
 	do {
-		crc = OW_crc8_table[ crc ^ *(p +i) ];
-	} while ( ++i < n );
+		crc = W1_crc8_table[ crc ^ *(p +i) ];
+	} while ( ++i < count );
 	return (crc);
 }
 
