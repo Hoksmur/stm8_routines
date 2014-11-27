2a7bd771f411e09c41c3c115fa366909c4c66eb2
Hoksmur <dragon-78@yandex.ru>
2014-11-27 22:10:11 +0700

    Update delay.h
    added SDCC and loopback for IAR and RAISONANCE

--- delay.h
+++ delay.h
@@ -1,5 +1,6 @@
 /* 
  * delay utilite for STM8 family
+ * COSMIC and SDCC
  * Terentiev Oleg
  * t.oleg@ymail.com
  */
@@ -19,13 +20,32 @@
 
 static inline void _delay_cycl( const unsigned short __ticks )
 {
+#if defined(__CSMC__)
+/* COSMIC */
+  #define T_COUNT(x) (( F_CPU * x / 1000000UL )-3)/3)
 	// ldw X, __ticks ; insert automaticaly
 	_asm("nop\n $N:\n decw X\n jrne $L\n nop\n ", __ticks);
+#elif defined(__SDCC)
+  #define T_COUNT(x) (( F_CPU * x / 1000000UL )-5)/5)
+	__asm__("nop\n nop\n"); 
+	do { 		// ASM: ldw X, #tick; lab$: decw X; tnzw X; jrne lab$
+                __ticks--;//      2c;                 1c;     2c    ; 1/2c   
+        } while ( __ticks );
+	__asm__("nop\n");
+#elif defined(__RCST7__)
+/* RAISONANCE */
+  #error ToDo for RAISONANCE
+#elif defined(__ICCSTM8__)
+/* IAR */
+  #error ToDo for IAR
+#else
+ #error Unsupported Compiler!          /* Compiler defines not found */
+#endif
 }
 
 static inline void _delay_us( const unsigned short __us )
 {
-	_delay_cycl( (unsigned short)(( F_CPU * __us / 1000000UL )-3)/3 );
+	_delay_cycl( (unsigned short)( T_COUNT(__us) );
 }
 
 static inline void _delay_ms( unsigned short __ms )
